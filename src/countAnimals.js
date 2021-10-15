2587e5714fbe3993557ab3702fa3645832ca264f
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-10-15 17:11:20 -0300

    Add requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      Object.assign(acc, { [specie.name]: specie.residents.length });
+      return acc;
+    }, {});
+  }
+
+  if (!animal.sex) {
+    return species
+      .find((specie) => specie.name === animal.specie)
+      .residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie)
+    .residents.filter((specieSex) => specieSex.sex === animal.sex)
+    .length;
 }
 
 module.exports = countAnimals;
