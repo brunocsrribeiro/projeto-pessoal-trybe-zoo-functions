6af31f62897a025399ccacecd6a8628745d0d826
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-10-16 14:09:55 -0300

    Add requirement 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,39 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const ageGroup = {
+    adult: 0,
+    senior: 0,
+    child: 0,
+  };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      ageGroup.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      ageGroup.adult += 1;
+    } else {
+      ageGroup.senior += 1;
+    }
+  });
+  return ageGroup;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const sum = [];
+  const total = (acc, curr) => acc + curr;
+  countEntrants(entrants);
+
+  sum.push(Object.values(countEntrants(entrants))[0] * Object.values(prices)[0]);
+
+  sum.push(Object.values(countEntrants(entrants))[1] * Object.values(prices)[1]);
+
+  sum.push(Object.values(countEntrants(entrants))[2] * Object.values(prices)[2]);
+
+  return sum.reduce(total);
 }
 
 module.exports = { calculateEntry, countEntrants };
